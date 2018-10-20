4bcf099b1763a79a85df9e42072d302c2f3d5676
RafalCzech91 <rafalczech91@gmail.com>
2018-10-20 10:01:35 +0200

    test

---
+++ src/main/java/com/czech/Main.java
@@ -0,0 +1,14 @@
+package com.czech;
+
+public class Main {
+    /**
+     * Hello world!
+     *
+     */
+
+        public static void main( String[] args )
+        {
+            System.out.println( "Hello World!" );
+        }
+
+}
