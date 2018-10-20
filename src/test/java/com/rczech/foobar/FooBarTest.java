c6f7b95422c3a84b29e7e842f8f561a1dc66ec3a
RafalCzech91 <rafalczech91@gmail.com>
2018-10-20 10:50:09 +0200

    FooBar create test to verifay int repesenationa as String

---
+++ src/test/java/com/rczech/foobar/FooBarTest.java
@@ -0,0 +1,29 @@
+package com.rczech.foobar;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FooBarTest {
+
+    @Test
+    public void shouldReturnStringIntRepresentationWhenPutIn() {
+
+
+        //given
+
+        int x = 1;
+
+        //when
+       String z = new FooBar().convertToString(x);
+
+        //then
+        Assert.assertEquals("1",z);
+
+
+
+
+    }
+
+
+}
+
