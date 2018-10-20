1cbb5154681cdd84415609bba83bd95d597702ea
RafalCzech91 <rafalczech91@gmail.com>
2018-10-20 11:36:45 +0200

    FooBar - test to verify Foo wil be returned when given int would be dividabele by trhree

--- src/test/java/com/rczech/FooBarTest.java
+++ src/test/java/com/rczech/FooBarTest.java
@@ -8,6 +8,8 @@ public class FooBarTest {
     public static final String EXPECTED_REPRESENTATION = "1";
     public static final int NUMBER = 1;
 
+
+
     @Test
     public void shouldReturnStringIntRepresentationWhenPutIn() {
 
@@ -18,13 +20,23 @@ public class FooBarTest {
        String result = new FooBar().convertToString(NUMBER);
 
 
-
         //then
         Assert.assertEquals(EXPECTED_REPRESENTATION,result);
 
 
 
 
+
+    }
+
+    @Test
+    public void shouldReturnStringFooWhenIntIsDividableByThree() {
+        //given
+        int number =3;
+        //when
+        String result = new FooBar().convertToString(number);
+        //then
+        Assert.assertEquals("Foo", result);
     }
 
 
