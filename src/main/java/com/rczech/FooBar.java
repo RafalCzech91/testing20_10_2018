002c78c8ca05578ba629edb27f37ca3509cef79f
RafalCzech91 <rafalczech91@gmail.com>
2018-10-20 11:39:54 +0200

    FooBar - implement second test for number = if three return "FOO"

--- src/main/java/com/rczech/FooBar.java
+++ src/main/java/com/rczech/FooBar.java
@@ -2,6 +2,9 @@ package com.rczech;
 
 public class FooBar {
     public String convertToString(int number) {
+        if(number%3 ==0) {
+            return  "Foo";
+        }
         return String.valueOf(number);
 
     }
